e9fa59a64ad52faababdeafb7a7fac23ffa8a26a
Aliaksandr Torkhau <aliaksandr@sageinclusion.com>
2025-05-10 20:15:05 +0200

    test: implement tests for throttledGetDataFromApi with axios and lodash mocks

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,53 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios, { AxiosInstance } from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+jest.mock('lodash', () => {
+  const originalModule = jest.requireActual('lodash');
+
+  return {
+    ...originalModule,
+    throttle: jest.fn((fn) => fn),
+  };
+});
+
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('throttledGetDataFromApi', () => {
+  const testData = { data: 'test data' };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    const mockGet = jest.fn().mockResolvedValue({ data: testData });
+    const mockAxiosInstance: Partial<AxiosInstance> = {
+      get: mockGet,
+    };
+
+    mockedAxios.create.mockReturnValue(mockAxiosInstance as AxiosInstance);
+  });
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    await throttledGetDataFromApi('/test');
+
+    expect(mockedAxios.create).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    await throttledGetDataFromApi('/test');
+    const mockInstance = mockedAxios.create.mock.results[0]
+      ?.value as AxiosInstance;
+    if (!mockInstance) {
+      throw new Error('Mock instance is undefined');
+    }
+    expect(mockInstance.get).toHaveBeenCalledWith('/test');
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const result = await throttledGetDataFromApi('/test');
+
+    expect(result).toEqual(testData);
   });
 });
